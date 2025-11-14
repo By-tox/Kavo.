50134c7f1ffc4dce9c9c67b4d07f2709f63ecaff
By-tox <medobytox@gmail.com>
2025-11-14 23:58:57 +0300

    Update and rename source.lua.txt to source.lua

--- source.lua
+++ source.lua
@@ -1,6 +1,12 @@
---// to the 2 peoplee who are constantly watching this repo, get a life yall weird.
---// to the people who are still forking this unoptimized garbage, if you want a custom optimized rewrite for $, hmu on discord: federal6768 or federal.
-
+--[[ 
+kavo ui edit bytox 
+ ____        _            
+| __ ) _   _| |_ _____  __
+|  _ \| | | | __/ _ \ \/ /
+| |_) | |_| | || (_) >  < 
+|____/ \__, |\__\___/_/\_\
+       |___/              
+--]]
 local Kavo = {}
 
 local tween = game:GetService("TweenService")
@@ -50,77 +56,77 @@ function Utility:TweenObject(obj, properties, duration, ...)
     tween:Create(obj, tweeninfo(duration, ...), properties):Play()
 end
 
-
 local themes = {
     SchemeColor = Color3.fromRGB(74, 99, 135),
-    Background = Color3.fromRGB(36, 37, 43),
-    Header = Color3.fromRGB(28, 29, 34),
-    TextColor = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(32, 32, 38)
+    Background = Color3.fromRGB(255, 255, 255),
+    Header = Color3.fromRGB(0, 0, 0),
+    TextColor = Color3.fromRGB(0, 0, 0),
+    ElementColor = Color3.fromRGB(0, 0, 0)
 }
+
 local themeStyles = {
     DarkTheme = {
         SchemeColor = Color3.fromRGB(64, 64, 64),
-        Background = Color3.fromRGB(0, 0, 0),
+        Background = Color3.fromRGB(255, 255, 255),
         Header = Color3.fromRGB(0, 0, 0),
-        TextColor = Color3.fromRGB(255,255,255),
-        ElementColor = Color3.fromRGB(20, 20, 20)
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     LightTheme = {
         SchemeColor = Color3.fromRGB(150, 150, 150),
-        Background = Color3.fromRGB(255,255,255),
-        Header = Color3.fromRGB(200, 200, 200),
-        TextColor = Color3.fromRGB(0,0,0),
-        ElementColor = Color3.fromRGB(224, 224, 224)
+        Background = Color3.fromRGB(255, 255, 255),
+        Header = Color3.fromRGB(0, 0, 0),
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     BloodTheme = {
         SchemeColor = Color3.fromRGB(227, 27, 27),
-        Background = Color3.fromRGB(10, 10, 10),
-        Header = Color3.fromRGB(5, 5, 5),
-        TextColor = Color3.fromRGB(255,255,255),
-        ElementColor = Color3.fromRGB(20, 20, 20)
+        Background = Color3.fromRGB(255, 255, 255),
+        Header = Color3.fromRGB(0, 0, 0),
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     GrapeTheme = {
         SchemeColor = Color3.fromRGB(166, 71, 214),
-        Background = Color3.fromRGB(64, 50, 71),
-        Header = Color3.fromRGB(36, 28, 41),
-        TextColor = Color3.fromRGB(255,255,255),
-        ElementColor = Color3.fromRGB(74, 58, 84)
+        Background = Color3.fromRGB(255, 255, 255),
+        Header = Color3.fromRGB(0, 0, 0),
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     Ocean = {
         SchemeColor = Color3.fromRGB(86, 76, 251),
-        Background = Color3.fromRGB(26, 32, 58),
-        Header = Color3.fromRGB(38, 45, 71),
-        TextColor = Color3.fromRGB(200, 200, 200),
-        ElementColor = Color3.fromRGB(38, 45, 71)
+        Background = Color3.fromRGB(255, 255, 255),
+        Header = Color3.fromRGB(0, 0, 0),
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     Midnight = {
         SchemeColor = Color3.fromRGB(26, 189, 158),
-        Background = Color3.fromRGB(44, 62, 82),
-        Header = Color3.fromRGB(57, 81, 105),
-        TextColor = Color3.fromRGB(255, 255, 255),
-        ElementColor = Color3.fromRGB(52, 74, 95)
+        Background = Color3.fromRGB(255, 255, 255),
+        Header = Color3.fromRGB(0, 0, 0),
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     Sentinel = {
         SchemeColor = Color3.fromRGB(230, 35, 69),
-        Background = Color3.fromRGB(32, 32, 32),
-        Header = Color3.fromRGB(24, 24, 24),
-        TextColor = Color3.fromRGB(119, 209, 138),
-        ElementColor = Color3.fromRGB(24, 24, 24)
+        Background = Color3.fromRGB(255, 255, 255),
+        Header = Color3.fromRGB(0, 0, 0),
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     Synapse = {
         SchemeColor = Color3.fromRGB(46, 48, 43),
-        Background = Color3.fromRGB(13, 15, 12),
-        Header = Color3.fromRGB(36, 38, 35),
-        TextColor = Color3.fromRGB(152, 99, 53),
-        ElementColor = Color3.fromRGB(24, 24, 24)
+        Background = Color3.fromRGB(255, 255, 255),
+        Header = Color3.fromRGB(0, 0, 0),
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     Serpent = {
         SchemeColor = Color3.fromRGB(0, 166, 58),
-        Background = Color3.fromRGB(31, 41, 43),
-        Header = Color3.fromRGB(22, 29, 31),
-        TextColor = Color3.fromRGB(255,255,255),
-        ElementColor = Color3.fromRGB(22, 29, 31)
+        Background = Color3.fromRGB(255, 255, 255),
+        Header = Color3.fromRGB(0, 0, 0),
+        TextColor = Color3.fromRGB(0, 0, 0),
+        ElementColor = Color3.fromRGB(0, 0, 0)
     }
 }
 local oldTheme = ""
